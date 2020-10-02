68ff17ce409ed90f0cf7f8defc6767ca8dba1661
luu.van <luu.van@hoanghacgroup.com>
2020-10-02 17:27:03 +0700

    in it

---
+++ src/context/AppReducer.js
@@ -0,0 +1,14 @@
+
+export default (state, action) => {
+    switch(action.type) {
+
+        case 'DELETE_TRANSACTION':
+            return {
+                ...state,
+                transactions: state.transactions.filter(transactions => transactions.id !== action.payload)
+
+            }
+        default:
+            return state;
+    }
+}
